6a562fb216d51dfd03cb71ea6462d4f5972e0140
Edison Bosnyak da Costa Junior <edison.junior@rain.us>
2023-01-11 16:53:10 -0300

    Feat: add createPlaylist function PlaylistRepository

--- src/repositories/playlist.ts
+++ src/repositories/playlist.ts
@@ -1,8 +1,10 @@
 import { v4 as uuidv4 } from 'uuid';
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 import {
+  PlaylistData,
   PlaylistRepositoryDependencies,
   IPlaylistRepository,
+  CreatePlaylistPayload,
 } from '../types';
 
 export default class PlaylistRepository implements IPlaylistRepository {
@@ -13,6 +15,28 @@ export default class PlaylistRepository implements IPlaylistRepository {
   constructor({ dynamoDbClient, config }: PlaylistRepositoryDependencies) {
     this.dynamoDbClient = dynamoDbClient;
     this.config = config;
+  }
+
+  async createPlaylist(payload: CreatePlaylistPayload) {
+    try {
+      const playlist: PlaylistData = {
+        ...payload,
+        id: uuidv4(),
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+      };
+      await this.dynamoDbClient.put({
+        TableName: this.config.playlistTableName,
+        Item: playlist,
+      }).promise();
+
+      return playlist;
+    } catch (err) {
+      if (err instanceof Error) {
+        console.log(`Failed to store playlist data into database (${err.message})`);
+      }
+      throw err;
+    }
   }
 
 }
