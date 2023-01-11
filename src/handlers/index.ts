d1674645dd476a8f412b882267df986a4658176b
Edison Bosnyak da Costa Junior <edison.junior@rain.us>
2023-01-11 17:28:32 -0300

    Feat: add getAllPlaylistsHandler handler

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,17 @@
 import { APIGatewayProxyEvent, APIGatewayProxyHandler } from 'aws-lambda';
 import repositories from '../repositories';
 import CreatePlaylistHandler from './createPlaylist';
+import GetAllPlaylistsHandler from './getAllPlaylists';
 const createPlaylistHandlerWrapper = new CreatePlaylistHandler({
   playlistRepository: repositories.playlistRepository,
 });
 
+const getAllPlaylistsHandlerWrapper = new GetAllPlaylistsHandler({
+  playlistRepository: repositories.playlistRepository,
+});
 export const createPlaylistHandler: APIGatewayProxyHandler = async (
   event: APIGatewayProxyEvent,
 ) => createPlaylistHandlerWrapper.handler(event);
+
+export const getAllPlaylistsHandler:
+  APIGatewayProxyHandler = async () => getAllPlaylistsHandlerWrapper.handler();
--- src/tests/unit/handlers/handler.spec.ts
+++ src/tests/unit/handlers/handler.spec.ts
@@ -1,12 +1,14 @@
 import { expect } from 'chai';
 import {
   createPlaylistHandler,
+  getAllPlaylistsHandler,
 } from '../../../handlers';
 
 describe('Handlers', () => {
   context('handlers function', () => {
     it('should instantiate all handlers successfully', async () => {
       expect(createPlaylistHandler).to.be.a('function');
+      expect(getAllPlaylistsHandler).to.be.a('function');
     });
   });
 });
